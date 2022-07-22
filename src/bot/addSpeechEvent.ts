891aff64a54daa70550ee1f959f4d77e4fb3197c
MidSpike <developer@midspike.com>
2022-07-22 19:17:04 -0400

    fix

--- src/bot/addSpeechEvent.ts
+++ src/bot/addSpeechEvent.ts
@@ -2,25 +2,56 @@ import prism from "prism-media";
 
 import { Client } from "discord.js";
 
-import { EndBehaviorType, getVoiceConnection, entersState, VoiceConnectionStatus, VoiceConnectionDisconnectReason } from "@discordjs/voice";
+import { EndBehaviorType, getVoiceConnection, entersState, VoiceConnection, VoiceConnectionStatus } from "@discordjs/voice";
 
 import { createVoiceMessage } from "./voiceMessage/createVoiceMessage";
 
 //------------------------------------------------------------//
 
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+function onSpeakingStartSpeechRecognition(client: Client, connection: VoiceConnection, userId: string) {
+  const user = client.users.cache.get(userId);
+  if (!user) return;
+  if (user.bot) return;
+  if (user.system) return;
 
-//------------------------------------------------------------//
+  const opusStream = connection.receiver.subscribe(userId, {
+    end: {
+      behavior: EndBehaviorType.AfterSilence,
+      duration: 100,
+    },
+  });
 
-const activeGuildVoiceListeners = new Set<string>();
+  const bufferData: Uint8Array[] = [];
+  opusStream.pipe(
+    new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 })
+  ).on("data", (data: Uint8Array) => {
+    bufferData.push(data);
+  });
+
+  opusStream.on("end", async () => {
+    const voiceMessage = await createVoiceMessage({
+      client,
+      bufferData,
+      user,
+      connection,
+    });
+
+    if (!voiceMessage) return;
+
+    client.emit("speech", voiceMessage);
+  });
+}
 
 //------------------------------------------------------------//
 
 export function addSpeechEvent(
-  client: Client,
+  client: Client<true>,
 ) {
   client.on<"voiceStateUpdate">("voiceStateUpdate", async (_old, newVoiceState) => {
+    if (newVoiceState.id !== client.user.id) return;
     if (!newVoiceState.channel) return;
+
+    // we can now assume that this voice state is for the bot
 
     const connection = getVoiceConnection(newVoiceState.channel.guild.id);
     if (!connection) return;
@@ -33,98 +64,13 @@ export function addSpeechEvent(
       return;
     }
 
-    const isGuildVoiceListenerActive = activeGuildVoiceListeners.has(connection.joinConfig.guildId);
+    const isGuildVoiceListenerActive = Boolean(
+      connection.receiver.speaking.listeners("start").find(
+        (func) => func.name === "onSpeakingStartSpeechRecognition"
+      )
+    );
     if (isGuildVoiceListenerActive) return;
-    activeGuildVoiceListeners.add(connection.joinConfig.guildId);
 
-    connection.on<"stateChange">("stateChange", async (oldState, newState) => {
-      switch (newState.status) {
-        case VoiceConnectionStatus.Disconnected: {
-          const disconnectedByWebsocket = newState.reason === VoiceConnectionDisconnectReason.WebSocketClose;
-          if (disconnectedByWebsocket && newState.closeCode === 4014) {
-            try {
-                await entersState(connection, VoiceConnectionStatus.Connecting, 5_000);
-            } catch {
-                connection.destroy();
-            }
-          }
-
-          /**
-           * Attempt to manually rejoin the voice connection.
-           * Wait 5 seconds before attempting each reconnect.
-           */
-          if (connection.rejoinAttempts < 5) {
-            await delay((connection.rejoinAttempts + 1) * 5_000);
-
-            connection.rejoin();
-
-            break;
-          }
-
-          break;
-        }
-
-        case VoiceConnectionStatus.Destroyed: {
-          activeGuildVoiceListeners.delete(connection.joinConfig.guildId);
-
-          break;
-        }
-
-        case VoiceConnectionStatus.Connecting:
-        case VoiceConnectionStatus.Signalling: {
-          try {
-            /**
-             * Wait 20 seconds for the connection to become ready before destroying the voice connection.
-             * This prevents the voice connection from permanently existing in one of these states.
-             */
-            await entersState(connection, VoiceConnectionStatus.Ready, 20_000);
-          } catch {
-            connection.destroy();
-
-            break;
-          }
-
-          break;
-        }
-
-        default: {
-          break;
-        }
-      }
-    });
-
-    connection.receiver.speaking.on("start", (userId) => {
-      const user = client.users.cache.get(userId);
-      if (!user) return;
-      if (user.bot) return;
-      if (user.system) return;
-
-      const opusStream = connection.receiver.subscribe(userId, {
-        end: {
-          behavior: EndBehaviorType.AfterSilence,
-          duration: 100,
-        },
-      });
-
-      const bufferData: Uint8Array[] = [];
-      opusStream.pipe(
-        new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 })
-      ).on("data", (data: Uint8Array) => {
-        bufferData.push(data);
-      });
-
-      opusStream.on("end", async () => {
-        const voiceMessage = await createVoiceMessage({
-          client,
-          bufferData,
-          user,
-          connection,
-        });
-
-        if (!voiceMessage) return;
-
-        client.emit("speech", voiceMessage);
-      });
-    });
+    connection.receiver.speaking.on("start", (userId) => onSpeakingStartSpeechRecognition(client, connection, userId));
   });
 };

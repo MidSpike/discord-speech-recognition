140842af20a136eeca08b11829c1b7206e050d61
MidSpike <developer@midspike.com>
2022-08-05 20:09:08 -0400

    forgot to export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { attachSpeechEvent } from './bot/attachSpeechEvent';
 export { VoiceMessage } from './bot/voiceMessage/VoiceMessage';
 
 export { resolveSpeechWithGoogleSpeechV2 } from './speechRecognition/googleV2';
+
+export { SpeechErrorCode, SpeechError } from './utils/speechError';
